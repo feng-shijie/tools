4874cc83e09b85cc4399801369eb18d405d3b7e0
feng-shijie <f_2315@tom.com>
2024-04-08 14:08:11 +0800

    [feature] 规范变量命名 优化类成员函数

--- c_c++/log/log.cpp
+++ c_c++/log/log.cpp
@@ -1,118 +1,101 @@
-#include"public.h"
-#include <string.h>
+#include "log.h"
 
-pthread_mutex_t		CIULOG::log_mutex;
-std::ofstream	CIULOG::fp;
-char*		CIULOG::fpname = new char[20];		//初始化静态char变量,静态字符串还是要申明指针
-bool		CIULOG::m_btoFile = true;		//默认输入到日志文件内
-bool		CIULOG::mutex_val = false;		//默认为单线程不需要使用互斥锁
-LOG_LEVEL	CIULOG::m_nloglevel = LOG_LEVEL_INFO;	//默认日志级别为信息输出
 //代码一运行就初始化创建实例,本身就线程安全
-CIULOG* CIULOG::ciulog = new(std::nothrow)CIULOG();
+CIULOG* CIULOG::m_only 	  = new(std::nothrow)CIULOG();
+pthread_mutex_t	CIULOG::m_mutex;
 
+CIULOG::CIULOG(){
+	m_fp		= new std::ofstream();
+	m_mode 		= false;			//默认为单线程不需要使用互斥锁
+	m_out_mode 	= true;				//默认输入到日志文件内
+	m_level 	= LOG_LEVEL_INFO;	//默认日志级别为信息输出
+}
 
 CIULOG::~CIULOG()
 {
-	deleteInstance();
-	fp.close();
-}
-
-//释放单列实例
-void CIULOG::deleteInstance()
-{
-	if(ciulog)
-	{
-		delete ciulog;
-		ciulog = NULL;
+	if(m_only){
+		delete m_only;
+		m_only = nullptr;
+	}
+	if(m_fp){
+		delete m_fp;
+		m_fp = nullptr;
 	}
 }
 
-//日志初始化,bool输出到文件还是控制台,bool长日志是否截断,PCTSTR统一字符串
-void CIULOG::init_ciulog(bool btofile, bool val)
+CIULOG * CIULOG::g(){
+	static std::mutex mu_lock;
+
+	if(m_only == nullptr){
+		mu_lock.lock();
+		if(m_only == nullptr)	m_only = new CIULOG();
+		mu_lock.unlock();
+	}
+	return m_only;
+}
+
+//日志初始化,_mode输出到文件还是控制台,_is_thread是否为多线程,PCTSTR统一字符串
+void CIULOG::init(bool _mode, bool _is_thread)
 {
-	m_btoFile = btofile;
-	mutex_val = val;
+	m_out_mode 	= _mode;
+	m_mode		= _is_thread;
 
-	if(m_btoFile == true)
-	{
-		struct timeval tv;	//把当地时间转换为秒
-		struct tm* ptm;		//当地时间(年月日,时分秒)
-		memset(fpname,0,sizeof(fpname));
-
-		gettimeofday(&tv,NULL);		//把当地时间转换为秒
-		ptm = localtime(&(tv.tv_sec));	//把秒转换为当地时间
-		//fpname是指针,指针大小固定为8
-		strftime(fpname,sizeof(char[20]), "%Y-%m-%d", ptm);
-
-		fp.open(fpname,std::ios::in);
-		if(!fp.is_open())
-		{
-			std::cout << "current init_ciulog() file open error\n";
-			return;
-		}
-		fp.close();
-
-		if(mutex_val)		//判断是否为多线程模式
-		{
-			if(pthread_mutex_init(&log_mutex,0) !=0)	//初始化互斥锁
-				LOG_ERROR("mutex init error");
-		}
+	if(_is_thread){
+		if(pthread_mutex_init(&m_mutex,0) !=0)	//初始化互斥锁
+			LOG_ERROR("mutex init error");
 	}
 
 }
-void CIULOG::setlevel(LOG_LEVEL level)  //修改日志级别
+void CIULOG::setlevel(LOG_LEVEL _level)  //设置日志级别
 {
-	m_nloglevel = level;
+	m_level = _level;
 }
 
-bool CIULOG::log(LOG_LEVEL level, const char* FileName, const char* FunctionName, int Row, const char* logstr,...)
+bool CIULOG::log(const char * _file_name, const char* _fun_name, int _row, LOG_LEVEL _level, const char * _logstr,...)
 {
-	if(mutex_val)	//判断是否为多线程
-	{
-		if(pthread_mutex_lock(&log_mutex) != 0)	//加锁
-		{
-			mutex_val = false;
+	//判断是否为多线程
+	if(m_mode){
+		if(pthread_mutex_lock(&m_mutex) != 0){	//加锁
+			m_mode = false;
 			LOG_ERROR("lock error");
 			return false;
 		}
 	}
 
-	char str[1024],levelname[10];
+	char str[2048],levelname[10];
 	memset(str,0,sizeof(str));
 	memset(levelname,0,sizeof(levelname));
 	
-	if(level == LOG_LEVEL_INFO)		strcpy(levelname, "INFO");
-	else if(level == LOG_LEVEL_WARNING)	strcpy(levelname, "WARNING");
-	else if(level == LOG_LEVEL_ERROR)	strcpy(levelname, "ERROR");
+	if(		_level == LOG_LEVEL_INFO	)	strcpy(levelname, "INFO");
+	else if(_level == LOG_LEVEL_WARNING )	strcpy(levelname, "WARNING");
+	else if(_level == LOG_LEVEL_ERROR	)	strcpy(levelname, "ERROR");
 
-	struct timeval tv;	//把当地时间转换为秒
-	struct tm* ptm;		//当地时间(年月日,时分秒)
 	char time_string[40];
+	time_t t_now = time(0);			//获取时间戳
+	tm * ltm = localtime(&t_now);	//转为tm结构体
+	strftime(time_string,sizeof(time_string), "%H:%M:%S", ltm);
 
-	gettimeofday(&tv,NULL);		//把当地时间转换为秒
-	ptm = localtime(&(tv.tv_sec));	//把秒转换为当地时间
-	strftime(time_string,sizeof(time_string), "%Y-%m-%d %H:%M:%S", ptm);
-
-	if(m_btoFile)
-	{
-		sprintf(str,"[%s][%s][PID:%u][TID:%u][%s][%s(): %d]::%s\n",time_string,levelname,
-		(unsigned int)getpid(),(unsigned int)gettid(),FileName,FunctionName,Row,logstr);
+	if(m_out_mode){
+		char c_name[10];
+		sprintf(c_name,"%s_%d", LOG_FILE, ltm->tm_mday);
+		sprintf(str,"[%s][%s][%s][%s(): %d]::%s\n",time_string, levelname,_file_name, _fun_name, _row, _logstr);
 		//每次写日志要打开文件
-		fp.open(fpname,std::ios::out | std::ios::app);
-		fp << str;
-		fp.close();	//写完后关闭文件
+		m_fp->open(c_name, std::ios::out | std::ios::app);
+		if(!m_fp->is_open()){
+			std::cout << "file:: " << c_name << " open fail\n";
+			return false;
+		}
+		*m_fp << str;
+		m_fp->close();	//写完后关闭文件
 	}
-	else
-	{
+	else{
 		printf("[%s][%s][PID:%u][TID:%u][%s][%s: %d]::%s\n",time_string,levelname,
-		(unsigned int)getpid(),(unsigned int)gettid(),FileName,FunctionName,Row,logstr);
+		(unsigned int)getpid(),(unsigned int)gettid(), _file_name, _fun_name, _row, _logstr);
 	}
 
-	if(mutex_val)
-	{
-		if(pthread_mutex_unlock(&log_mutex) != 0)
-		{
-			mutex_val = false;
+	if(m_mode){
+		if(pthread_mutex_unlock(&m_mutex) != 0){
+			m_mode = false;
 			LOG_ERROR("unlock error");
 			return false;
 		}
--- c_c++/log/log.h
+++ c_c++/log/log.h
@@ -4,10 +4,9 @@
 *@date:		2023.1.26
 */
 
-#ifndef PUBLC_H
-#define PUBLIC_H 1
+#ifndef LOG_H
+#define LOG_H 1
 
-//socket服务端
 #include<iostream>
 #include<stdio.h>
 #include<stdlib.h>
@@ -16,10 +15,15 @@
 #include<sys/time.h>
 #include<sys/syscall.h>
 #include<fstream>
+#include <string.h>
+#include <thread>
+#include <mutex>
+#include <ctime>
 
 /*********************日志系统*******************************/
 
-#define gettid()	syscall(SYS_gettid)
+#define LOG_FILE "log"
+#define gettid()  syscall(SYS_gettid)
 
 //extern std::ofstream fp;	//外部变量声明如此使用
 
@@ -30,16 +34,17 @@ enum LOG_LEVEL	//日志级别
 	LOG_LEVEL_ERROR		//错误
 };
 
+
 //__FILE__::获取当前文件名, __FUNCTION__::获取当前函数名, __LINE__::获取当前所在行数, __VA_ARGS__::可变参数列表
-#define LOG_INFO(...)           CIULOG::log(LOG_LEVEL_INFO,__FILE__,__FUNCTION__,__LINE__,__VA_ARGS__)
-#define LOG_WARNING(...)        CIULOG::log(LOG_LEVEL_WARNING,__FILE__,__FUNCTION__,__LINE__,__VA_ARGS__)
-#define LOG_ERROR(...)          CIULOG::log(LOG_LEVEL_ERROR,__FILE__,__FUNCTION__,__LINE__,__VA_ARGS__)
+#define LOG_INFO(...)           CIULOG::log(__FILE__,__FUNCTION__,__LINE__,LOG_LEVEL_INFO,__VA_ARGS__)
+#define LOG_WARNING(...)        CIULOG::log(__FILE__,__FUNCTION__,__LINE__,LOG_LEVEL_WARNING,__VA_ARGS__)
+#define LOG_ERROR(...)          CIULOG::log(__FILE__,__FUNCTION__,__LINE__,LOG_LEVEL_ERROR,__VA_ARGS__)
 
 //日志系统
 class CIULOG
 {
 private:
-	CIULOG(){}
+	CIULOG();
 	~CIULOG();
 	CIULOG(const CIULOG &ciulog);
 	const CIULOG &operator = (const CIULOG &ciulog);
@@ -50,20 +55,19 @@ private:
 	//CIULOG& operator = (const CIULOG& rhs) = delete;	//阻止赋值
 
 private:
-	static bool		m_btoFile;	//日志写入文件还是控制台
-	static bool		mutex_val;	//是否为多线程模式
-	static LOG_LEVEL	m_nloglevel;	//日志级别
-	static char		*fpname;	//日志文件名
-	static CIULOG		*ciulog;	//唯一单实列对象指针
-	static pthread_mutex_t	log_mutex;	//互斥锁
-	static std::ofstream	fp;
+	bool		m_out_mode;	//日志写入文件还是控制台
+	bool		m_mode;		//是否为多线程模式
+	LOG_LEVEL	m_level;	//日志级别
+	std::ofstream *	m_fp;
+
+	static CIULOG *			m_only;		//唯一单实列对象指针
+	static pthread_mutex_t	m_mutex;	//互斥锁
 public:
-	static CIULOG *getInstance()	{return ciulog;}	//获取单列实例
-	static void deleteInstance();	//释放单列实例
+	static CIULOG * g();	//获取单列实例
 	//日志初始化,bool输出到文件还是控制台,bool长日志是否截断,PCTSTR统一字符串
-	static void init_ciulog(bool btofile, bool bTruncateLongLog);
-	static void setlevel(LOG_LEVEL level);	//修改日志级别
-	static bool log(LOG_LEVEL level, const char* FileName, const char* FunctionName, int Row, const char* logstr,...);
+	void init(bool _mode, bool _is_thread);
+	void setlevel(LOG_LEVEL _level);	//修改日志级别
+	bool log(const char * _file_name, const char* _fun_name, int _row, LOG_LEVEL _level, const char* _logstr,...);
 
 };
 
